abbe7aa88cfbe494ea07ec9ce93acae8ea316076
Jaime Spacco <jspacco@knox.edu>
2022-05-01 17:30:33 -0500

    removed unused imports

--- src/plagdetect/PlagiarismDetector.java
+++ src/plagdetect/PlagiarismDetector.java
@@ -3,11 +3,7 @@ package plagdetect;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
 
 public class PlagiarismDetector implements IPlagiarismDetector {
 	
